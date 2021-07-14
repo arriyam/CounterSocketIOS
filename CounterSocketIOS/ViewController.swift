5c15654441671112773b80a4f8b06644b020ba79
Arriyam <thushen@arriyam.com>
2021-07-13 20:35:18 -0400

    Initial Commit

---
+++ CounterSocketIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CounterSocketIOS
+//
+//  Created by Thushen Mohanarajah on 2021-07-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
